937d1c31701a29ababe50d8a9c871fa3604fb567
lcc19941214 <245987239@qq.com>
2019-12-11 19:01:53 +0800

    update README.md

--- src/nodejs/child_process/fork/main.js
+++ src/nodejs/child_process/fork/main.js
@@ -1,23 +1,27 @@
 const cp = require('child_process');
-const child = cp.fork('./child.js');
+const path = require('path');
 
 console.log('[main] pid: ' + process.pid);
-console.log('[main] child pid: ' + child.pid);
 
-child.on('message', msg => {
-  console.log('[main] got a message');
-  console.log('[main] message: ' + msg);
-});
+for (let index = 0; index < 2; index++) {
+  const child = cp.fork(path.resolve(__dirname, 'child.js'));
+  console.log('[main] child pid: ' + child.pid);
 
-// 监听子进程断开连接
-child.on('exit', () => {
-  console.log('[main] child process exited');
-});
+  child.on('message', msg => {
+    console.log('[main] got a message');
+    console.log('[main] message: ' + msg);
+  });
 
-console.log('[main] sent a message');
-child.send('hello world');
+  // 监听子进程断开连接
+  child.on('exit', () => {
+    console.log('[main] child process exited');
+  });
 
-setTimeout(() => {
-  child.disconnect();
-  // process.kill(child.pid)
-}, 3000);
+  console.log('[main] sent a message');
+  child.send('hello world');
+
+  setTimeout(() => {
+    child.disconnect();
+    // process.kill(child.pid)
+  }, 3000);
+}
